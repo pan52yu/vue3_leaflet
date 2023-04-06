62077d566f38de04f0f4fdb7fc04b1f80260a2b0
huyu <yhu8836@gmail.com>
2023-04-06 16:44:00 +0800

    feat: add vueuse

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,6 +11,11 @@ const routes: Array<RouteRecordRaw> = [
     },
     component: () => import('../views/home/index.vue'),
   },
+  {
+    path: '/vueuse',
+    name: 'vueUse',
+    component: () => import('../views/vueuse/index.vue'),
+  },
 ]
 
 const router = createRouter({

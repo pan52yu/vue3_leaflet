9d902e3432bbbcca3e73ba0a23abaa4475a6da1b
huyu <yhu8836@gmail.com>
2023-04-06 16:38:12 +0800

    feat: add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
-    <HelloWorld />
+    <RouterView></RouterView>
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import store from './store'
+import router from './router/index'
 const app = createApp(App)
 
 // 挂载Pinia
 app.use(store)
+app.use(router)
 
 app.mount('#app')

3e93a398f9c184734fbf289427e741d65c551a57
huyu <yhu8836@gmail.com>
2023-04-11 09:03:09 +0800

    下钻杭州

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,80 +2,102 @@
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import zhejiang from './zhejiang.json'
+import hangzhou from './hangzhou.json'
+
 defineOptions({
   name: 'LeafletMap',
 })
 const mapContainer = ref(null)
-
-onMounted(() => {
-  // 初始化地图
-  // const map = L.map(mapContainer.value, {
-  //   center: [30.245927, 120.154798],
-  //   zoom: 8,
-  // })
-  const map = L.map(mapContainer.value, {
+const map = ref<any>(null)
+const zheJiangLayer = ref<any>(null)
+const areaLayer = ref<any>(null)
+const initMap = () => {
+  map.value = L.map(mapContainer.value, {
     center: [30.245927, 120.154798],
     zoom: 8,
     maxZoom: 10,
     minZoom: 7,
     zoomControl: false,
-    style: 'background-color: transparent;', // 设置背景色为透明
   })
 
-  const onEachFeature = function (feature, layer) {
-    const center = layer.getBounds().getCenter()
-    const label = L.marker(center, {
-      icon: L.divIcon({
-        className: 'label',
-        html: feature.properties.name,
-        iconSize: [100, 20],
-      }),
-    }).addTo(map)
-    layer.on({
-      // 鼠标移入事件
-      mouseover: function () {
-        // 高亮显示
-        layer.setStyle({
-          weight: 5,
-          color: '#666',
-          dashArray: '',
-          fillOpacity: 0.7,
-        })
-        // 鼠标移入时，将当前图层置于最上层
-        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-          layer.bringToFront()
-        }
-      },
-      // 鼠标移出事件
-      mouseout: function () {
-        // 鼠标移出时，恢复默认样式
-        zheJiangLayer.resetStyle(this)
-      },
-      // 点击事件
-      click: function () {
-        // 自适应地图视图
-        map.fitBounds(layer.getBounds())
-        console.log(feature.properties.name)
-      },
-    })
-  }
-
-  L.tileLayer('', {
-    attribution: '&copy; <a href="javaScript:;">橙晖科技提供技术支持</a>',
-  }).addTo(map)
-
-  const zheJiangData = zhejiang
-  const zheJiangLayer = L.geoJSON(zheJiangData, {
+  zheJiangLayer.value = L.geoJSON(zhejiang, {
     onEachFeature: onEachFeature,
-  }).addTo(map)
+  }).addTo(map.value)
 
-  map.fitBounds(zheJiangLayer.getBounds())
+  map.value.fitBounds(zheJiangLayer.value.getBounds())
+}
+const onEachFeature = (feature, layer) => {
+  const center = layer.getBounds().getCenter()
+  const label = L.marker(center, {
+    icon: L.divIcon({
+      className: 'label',
+      html: feature.properties.name,
+      iconSize: [100, 20],
+    }),
+  }).addTo(map.value)
+  layer.on({
+    mouseover: function () {
+      layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7,
+      })
+      if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront()
+      }
+    },
+    mouseout: function () {
+      zheJiangLayer.value.resetStyle(this)
+    },
+    click: function () {
+      console.log(feature.properties.name)
+      if (feature.properties.name === '杭州市') {
+        clearMap()
+        areaLayer.value = L.geoJSON(hangzhou, {
+          // 添加杭州地图图层
+          onEachFeature: onEachFeature,
+        }).addTo(map.value)
+        map.value.fitBounds(areaLayer.value.getBounds()) // 自适应地图视图到市区地图
+      }
+    },
+  })
+}
+
+// 重置地图
+const resetMap = () => {
+  clearMap()
+  zheJiangLayer.value = L.geoJSON(zhejiang, {
+    // 添加杭州地图图层
+    onEachFeature: onEachFeature,
+  }).addTo(map.value)
+  map.value.fitBounds(zheJiangLayer.value.getBounds()) // 自适应地图视图到浙江地图
+}
+
+// 清空地图
+const clearMap = () => {
+  map.value.eachLayer(function (layer) {
+    if (layer instanceof L.Marker) {
+      map.value.removeLayer(layer)
+    }
+  })
+  if (zheJiangLayer.value) {
+    zheJiangLayer.value.clearLayers() // 清除浙江地图图层
+  }
+  if (areaLayer.value) {
+    areaLayer.value.clearLayers() // 清除杭州地图图层
+  }
+}
+
+onMounted(() => {
+  initMap()
 })
 </script>
 
 <template>
   <div class="leaflet_map">
     <div ref="mapContainer" class="mapContainer"></div>
+    <button @click="resetMap">重置</button>
   </div>
 </template>
 
@@ -83,13 +105,14 @@ onMounted(() => {
 .leaflet_map {
   width: 100vw;
   height: 100vh;
-  //background: #646cff;
 }
+
 .mapContainer {
-  width: 100vw;
-  height: 100vh;
+  width: 90vw;
+  height: 90vh;
   background: transparent;
 }
+
 .label {
   font-size: 14px;
   text-align: center;

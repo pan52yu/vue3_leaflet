119d4fd714582bd676936bec2dbb4a911a4e778f
huyu <yhu8836@gmail.com>
2023-04-11 11:37:26 +0800

    热力图

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,25 +1,41 @@
 <script setup lang="ts">
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
+// import 'leaflet/dist/leaflet.choropleth'
 import zhejiang from './areaJson/zhejiang.json'
-// import hangzhou from './hangzhou.json'
-// 动态引入杭州的JSON数据
-// const hangzhou = () => import('./hangzhou.json')
 
 defineOptions({
   name: 'LeafletMap',
 })
+
 const mapContainer = ref(null)
 const map = ref<any>(null)
 const zheJiangLayer = ref<any>(null)
-const mapStyle = {
-  weight: 1,
-  fillOpacity: 0.6,
-}
+
 const chineseToEnglishMap = [
   { name: '杭州市', value: 'hangzhou' },
   { name: '温州市', value: 'wenzhou' },
 ]
+
+// 随机生成0-400的数据
+const randomData = () => {
+  return Math.round(Math.random() * 400)
+}
+// 每个地区的数据，用于热力图展示
+const areaData = [
+  { name: '杭州市', value: randomData() },
+  { name: '温州市', value: randomData() },
+  { name: '湖州市', value: randomData() },
+  { name: '嘉兴市', value: randomData() },
+  { name: '宁波市', value: randomData() },
+  { name: '绍兴市', value: randomData() },
+  { name: '金华市', value: randomData() },
+  { name: '衢州市', value: randomData() },
+  { name: '台州市', value: randomData() },
+  { name: '丽水市', value: randomData() },
+  { name: '舟山市', value: randomData() },
+]
+
 // 异步加载地图
 const loadMap = async (area: string) => {
   const areaName = chineseToEnglishMap.find((item) => item.name === area)?.value
@@ -30,6 +46,7 @@ const loadMap = async (area: string) => {
   const url = `./areaJson/${areaName}.json`
   return import(url) // 返回地图数据
 }
+
 const initMap = () => {
   map.value = L.map(mapContainer.value, {
     center: [30.245927, 120.154798],
@@ -41,11 +58,43 @@ const initMap = () => {
 
   zheJiangLayer.value = L.geoJSON(zhejiang, {
     onEachFeature: onEachFeature,
-    style: mapStyle,
+    style: styleMap,
   }).addTo(map.value)
 
   map.value.fitBounds(zheJiangLayer.value.getBounds())
 }
+
+const styleMap = (feature: any) => {
+  const area = feature.properties.name
+  const areaValue = areaData.find((item) => item.name === area)?.value
+  const fillColor = areaValue ? getColor(areaValue) : '#2772d3'
+
+  return {
+    fillColor: fillColor,
+    weight: 1,
+    opacity: 1,
+    color: 'white',
+    dashArray: '3',
+    fillOpacity: 0.7,
+  }
+}
+
+const getColor = (value: number) => {
+  // 0-100 为白色，100-200 为蓝色，200-300 为橙色，300-400 为红色
+  switch (true) {
+    case value <= 100:
+      return '#ffffff'
+    case value <= 200:
+      return '#2772d3'
+    case value <= 300:
+      return '#f7a35c'
+    case value <= 400:
+      return '#f15c80'
+    default:
+      return '#ffffff'
+  }
+}
+
 const onEachFeature = (feature: any, layer: any) => {
   const center = layer.getBounds().getCenter()
   const label = L.marker(center, {
@@ -55,6 +104,7 @@ const onEachFeature = (feature: any, layer: any) => {
       iconSize: [100, 20],
     }),
   }).addTo(map.value)
+
   layer.on({
     mouseover: function () {
       layer.setStyle({
@@ -85,7 +135,7 @@ const onEachFeature = (feature: any, layer: any) => {
       zheJiangLayer.value = L.geoJSON(area, {
         // 添加杭州地图图层
         onEachFeature: onEachFeature,
-        style: mapStyle,
+        style: styleMap,
       }).addTo(map.value)
 
       map.value.fitBounds(zheJiangLayer.value.getBounds()) // 自适应地图视图
@@ -98,7 +148,7 @@ const resetMap = () => {
   clearMap()
   zheJiangLayer.value = L.geoJSON(zhejiang, {
     onEachFeature: onEachFeature,
-    style: mapStyle,
+    style: styleMap,
   }).addTo(map.value)
   map.value.fitBounds(zheJiangLayer.value.getBounds()) // 自适应地图视图
 }
@@ -131,7 +181,7 @@ onMounted(() => {
 .leaflet_map {
   width: 100vw;
   height: 100vh;
-  background: #164aa6;
+  background: #8a6060;
 }
 
 .mapContainer {
